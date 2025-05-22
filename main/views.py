071570135b0fe8581a078e51940ce34472254c93
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-05-22 23:28:43 +0500

    fff

--- main/views.py
+++ main/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,template_name='index.html')
+
+
+def toqmi_juftmi(son):
+    if son % 2 == 0:
+        return "Juft"
+    else:
+        return "Toq"
