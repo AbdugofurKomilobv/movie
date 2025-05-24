79fd7603758ca01dd74e5f523158f2c584ce9543
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-05-24 22:29:26 +0500

    gg

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,13 @@
 
 from django.urls import path,include
 from main.views import home
+from rest_framework.routers import DefaultRouter
+from .views import TaskViewSet
 
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet)
 urlpatterns = [
 
     path('home/',home ),
+     path('', include(router.urls)),
 ]
