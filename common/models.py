a5f021069e5997c77502e359f3f1b3d6d64ac28b
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-05-27 20:33:46 +0500

    fff

--- common/models.py
+++ common/models.py
@@ -14,3 +14,13 @@ class Article(models.Model):
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     published_at = models.DateTimeField(auto_now_add=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
