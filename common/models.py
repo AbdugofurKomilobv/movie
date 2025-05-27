6d8545e526fc9228884729233e21a9dd023a8aee
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-05-27 20:33:21 +0500

    fff

--- common/models.py
+++ common/models.py
@@ -6,3 +6,11 @@ class UserProfile(models.Model):
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
     bio = models.TextField(blank=True)
     birth_date = models.DateField(null=True, blank=True)
+
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    published_at = models.DateTimeField(auto_now_add=True)
