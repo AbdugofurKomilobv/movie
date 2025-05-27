58a84588a68e49956b5f37e99ea78fcf8a5965cd
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-05-27 20:35:17 +0500

    ff

--- common/models.py
+++ common/models.py
@@ -45,6 +45,11 @@ class Enrollment(models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     enrolled_at = models.DateTimeField(auto_now_add=True)
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
 
 
 
